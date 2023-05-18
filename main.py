f35dd2a0f7ec64befde358cd6f172905318bb297
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-05-18 16:33:20 +0800

    sessions are complicated, removed
    - an idiot admires complexity

--- main.py
+++ main.py
@@ -276,23 +276,11 @@ async def tic(ctx: commands.Context):
 
 # bard
 from bardapi import Bard
-import requests
-
-session = requests.Session()
-session.headers = {
-            "Host": "bard.google.com",
-            "X-Same-Domain": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "Origin": "https://bard.google.com",
-            "Referer": "https://bard.google.com/",
-        }
-session.cookies.set("__Secure-1PSID", os.getenv("BARD"))
-bardSession = Bard(session=session, timeout=60)
 
 @bot.command()
 async def bard(ctx, *, arg):
-    response = bardSession.get_answer(arg)['content']
+    bard = Bard(token=os.getenv("BARD"))
+    response = bard.get_answer(arg)['content']
     await ctx.reply(response[:2000])
 
 bot.run(os.getenv("TOKEN"))
