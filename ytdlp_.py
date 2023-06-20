078db8003fd1f079781ce8e5d3d4d20b455518e7
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-06-20 13:11:21 +0800

    notice
    prepared successfully

--- ytdlp_.py
+++ ytdlp_.py
@@ -5,26 +5,25 @@ import os
 
 async def YTDLP(ctx: commands.Context, arg1: str, arg2: str):
     formats = ['mp3', 'm4a']
-    if arg2 and not arg1 in formats: return await ctx.reply(f"Unsupported format :(")
+    if arg2 and not arg1 in formats: return await ctx.reply(f"Unsupported format :(\nAvailable conversion formats: `{formats}`")
     elif not arg2: arg2, arg1 = arg1, None
     ydl_opts = get_ydl_opts(arg1)
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(arg2, download=False)
         filename = ydl.prepare_filename(info_dict) if not arg1 else f"{os.path.splitext(ydl.prepare_filename(info_dict))[0]}.{arg1}"
-        await ctx.reply(f"Preparing `{filename}`\nLet me cook.")
+        msg = await ctx.reply(f"Preparing `{filename}`\nLet me cook.")
         ydl.download(arg2) # this is faulty
         if os.path.isfile(filename):
-            try: await ctx.reply(file=discord.File(filename))
-            except: await ctx.reply(f"Error: An error occured while cooking `{filename}`\nFile too large!")
+            try: 
+                await ctx.reply(content=None, file=discord.File(filename))
+                await msg.edit(content=f"`{filename}` has been prepared successfully!")
+            except: await msg.edit(content=f"Error: An error occured while cooking `{filename}`\nFile too large!")
             os.remove(filename)
         else: 
-            await ctx.reply(f"Error: An error occured while cooking `{filename}`\nFile too large!")
+            await msg.edit(content=f"Error: An error occured while cooking `{filename}`\nFile too large!")
 
 def checkSize(info, *, incomplete):
-    filesize = info.get('filesize')
-    if filesize and filesize > 25000000: # 25mb
-        return f'File too large! {filesize} bytes'
-    filesize = info.get('filesize_approx')
+    filesize = info.get('filesize') if info.get('filesize') else info.get('filesize_approx')
     if filesize and filesize > 25000000: # 25mb
         return f'File too large! {filesize} bytes'
 
@@ -43,6 +42,7 @@ def get_ydl_opts(arg):
     
     elif arg in video_formats: # disabled
         return {
+            'match_filter': checkSize,
             'postprocessors': [{
                 'key': 'FFmpegVideoConvertor',
                 'preferedformat': arg,
