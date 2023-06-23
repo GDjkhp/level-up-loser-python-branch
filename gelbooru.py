21526e318a53be264c5fb8971aaa959d3220dfcb
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-06-23 20:09:09 +0800

    lock
    - lock messages because it's annoying

--- gelbooru.py
+++ gelbooru.py
@@ -17,7 +17,8 @@ async def R34(ctx: commands.Context, arg: str):
         await message.edit(content=f"Searching posts with tags `{tags}`\nPlease wait…\n{len(results)} found")
         page+=1
     if len(results) == 0: return await message.edit(content="**No results found**")
-    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, False), view = ImageView(tags, results, 0, False))
+    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, False, [False, False], ctx), 
+                       view = ImageView(tags, results, 0, False, [False, False], ctx))
 
 async def GEL(ctx: commands.Context, arg: str):
     if not ctx.channel.nsfw: return await ctx.reply("**No.**")
@@ -32,7 +33,8 @@ async def GEL(ctx: commands.Context, arg: str):
         await message.edit(content=f"Searching posts with tags `{tags}`\nPlease wait…\n{len(results)} found")
         page+=1
     if len(results) == 0: return await message.edit(content="**No results found**")
-    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, False), view = ImageView(tags, results, 0, False))
+    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, False, [False, False], ctx), 
+                       view = ImageView(tags, results, 0, False, [False, False], ctx))
 
 async def SAFE(ctx: commands.Context, arg: str):
     tags = re.split(r'\s*,\s*', arg)
@@ -46,9 +48,10 @@ async def SAFE(ctx: commands.Context, arg: str):
         await message.edit(content=f"Searching posts with tags `{tags}`\nPlease wait…\n{len(results)} found")
         page+=1
     if len(results) == 0: return await message.edit(content="**No results found**")
-    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, True), view = ImageView(tags, results, 0, True))
+    await message.edit(content=None, embed = await BuildEmbed(tags, results, 0, True, [False, False], ctx), 
+                       view = ImageView(tags, results, 0, True, [False, False], ctx))
 
-async def BuildEmbed(tags: list, results, index: int, safe: bool) -> discord.Embed():
+async def BuildEmbed(tags: list, results, index: int, safe: bool, lock: list, ctx: commands.Context) -> discord.Embed():
     embed = discord.Embed(title=f"Search results: `{tags}`", description=f"{index+1}/{len(results)} found", color=0x00ff00)
     # if safe and not await Gelbooru(api='https://safebooru.org/').is_deleted(results[index].hash): 
     #     embed.add_field(name="This post was deleted.", value=results[index].hash)
@@ -60,21 +63,31 @@ async def BuildEmbed(tags: list, results, index: int, safe: bool) -> discord.Emb
     return embed
 
 class ImageView(discord.ui.View):
-    def __init__(self, tags, results, index, safe):
+    def __init__(self, tags: list, results: list, index: int, safe: bool, lock: list, ctx: commands.Context):
         super().__init__(timeout=None)
         if not index == 0: 
-            self.add_item(ButtonAction(tags, safe, results, 0, "⏪", 0))
-            self.add_item(ButtonAction(tags, safe, results, index - 1, "◀️", 0))
+            self.add_item(ButtonAction(tags, safe, results, 0, "⏪", 0, lock, ctx))
+            self.add_item(ButtonAction(tags, safe, results, index - 1, "◀️", 0, lock, ctx))
         if index + 1 < len(results): 
-            self.add_item(ButtonAction(tags, safe, results, index + 1, "▶️", 0))
-            self.add_item(ButtonAction(tags, safe, results, len(results)-1, "⏩", 0))
-        self.add_item(ButtonAction(tags, safe, results, random.randrange(0, len(results)), "🔀", 1))
+            self.add_item(ButtonAction(tags, safe, results, index + 1, "▶️", 0, lock, ctx))
+            self.add_item(ButtonAction(tags, safe, results, len(results)-1, "⏩", 0, lock, ctx))
+        self.add_item(ButtonAction(tags, safe, results, random.randrange(0, len(results)), "🔀", 1, lock, ctx))
+        self.add_item(ButtonAction(tags, safe, results, index, "🔒" if not lock[1] else "🔓", 1, [lock[1], not lock[1]], ctx))
 
 class ButtonAction(discord.ui.Button):
-    def __init__(self, tags, safe, results, index, l, row):
+    def __init__(self, tags: list, safe: bool, results: list, index: list, l: str, row: int, lock: list, ctx: commands.Context):
         super().__init__(emoji=l, style=discord.ButtonStyle.success, row=row)
-        self.results, self.index, self.tags, self.safe = results, index, tags, safe
+        self.results, self.index, self.tags, self.safe, self.lock, self.ctx = results, index, tags, safe, lock, ctx
     
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.edit_message(embed = await BuildEmbed(self.tags, self.results, self.index, self.safe),
-                                                view = ImageView(self.tags, self.results, self.index, self.safe))
+        if self.lock[1] and interaction.user != self.ctx.author: 
+            return await interaction.response.send_message(f"<@{self.ctx.message.author.id}> locked this message.", ephemeral=True)
+        if self.lock[0] != self.lock[1]:
+            if interaction.user != self.ctx.author:
+                return await interaction.response.send_message(f"Only <@{self.ctx.message.author.id}> can lock/unlock this message.", 
+                                                               ephemeral=True)
+            else: self.lock = [self.lock[1], self.lock[1]]
+        await interaction.response.edit_message(embed = await BuildEmbed(self.tags, self.results, self.index, self.safe, self.lock, self.ctx), 
+                                                view = ImageView(self.tags, self.results, self.index, self.safe, self.lock, self.ctx))
+        
+        
