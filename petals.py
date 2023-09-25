bb2557902c4a0dfda6bad1c5946916887dafa6cb
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-09-25 13:11:09 +0800

    petals websocket
    - optimizations :thumbsup:

--- petals.py
+++ petals.py
@@ -1,32 +1,57 @@
 from discord.ext import commands
 import time
-import aiohttp
 import requests
+import websockets
+import json
 
-async def petal(ctx: commands.Context, arg: str, model: str):
+# TODO: support read replies
+async def petalsWebsocket(ctx: commands.Context, arg: str, model: str):
+    """
+    Connects to a WebSocket server and generates text using a specified model.
+
+    This function connects to the WebSocket server at 'wss://chat.petals.dev/api/v2/generate',
+    opens an inference session with the 'stabilityai/StableBeluga2' model, and generates text based on
+    the given prompt.
+
+    Returns:
+        None
+    """
     async with ctx.typing():  # Use async ctx.typing() to indicate the bot is working on it.
+        if not arg: arg = "Explain who you are, your functions, capabilities, limitations, and purpose."
         msg = await ctx.reply("Generating response…")
+        text = ""
         old = round(time.time() * 1000)
-        if not arg:
-            arg = "Explain who you are, your functions, capabilities, limitations, and purpose."
-        
-        data = {
-            'model': model,
-            'inputs': arg,
-            'max_length': 512,
-            'do_sample': 1,
-            'temperature': 0.9,
-            'top_p': 0.6,
-        }
+        uri = "wss://chat.petals.dev/api/v2/generate"
+        max_length = 512
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post('https://chat.petals.dev/api/v1/generate', data=data) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    if data["ok"]:
-                        if not data["outputs"]:
-                            return await msg.edit(content=f"**Error! :(**\nEmpty response.")
-                        chunks = [data["outputs"][i:i+2000] for i in range(0, len(data["outputs"]), 2000)]
+        async with websockets.connect(uri) as ws:
+            await ws.send(json.dumps({
+                "type": "open_inference_session",
+                "model": model,
+                "max_length": max_length
+            }))
+            
+            await ws.send(json.dumps({
+                "type": "generate",
+                "inputs": f"A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.###Assistant: Hi! How can I help you?###Human: {arg}###Assistant:",
+                "max_new_tokens": 1,
+                "do_sample": 1,
+                "temperature": 0.6,
+                "top_p": 0.9,
+                "extra_stop_sequences": ["</s>"],
+                "stop_sequence": "###"
+            }))
+
+            async for message in ws:
+                data = json.loads(message)
+                if data.get("ok"):
+                    if data.get("outputs") is None:
+                        await msg.edit(content="**Session opened, generating...**")
+                    elif not data["stop"]:
+                        text += data["outputs"]
+                        await msg.edit(content=f"**Session opened, generating...**\nLength: {len(text)}")
+                    else: 
+                        chunks = [text[i:i+2000] for i in range(0, len(text), 2000)]
                         replyFirst = True
                         for chunk in chunks:
                             if replyFirst: 
@@ -34,29 +59,30 @@ async def petal(ctx: commands.Context, arg: str, model: str):
                                 await ctx.reply(chunk)
                             else:
                                 await ctx.send(chunk)
-                        await msg.edit(content=f"**Took {round(time.time() * 1000)-old}ms**")
-                    else:
-                        await msg.edit(content=f"**Error! :(**\n{PETALS()}")
+                        await msg.edit(content=f"**Took {round(time.time() * 1000)-old}ms**\nLength: {len(text)}")
+                        await ws.close()
                 else:
-                    await msg.edit(content=f"**Error! :(**")
+                    print("Error:", data.get("traceback"))
+                    await msg.edit(content=f"**Error! :(**\n{PETALS()}")
+                    await ws.close()
 
 async def BELUGA2(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'stabilityai/StableBeluga2')
+    await petalsWebsocket(ctx, arg, 'stabilityai/StableBeluga2')
 
 async def LLAMA2(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'meta-llama/Llama-2-70b-chat-hf') # meta-llama/Llama-2-70b-hf
+    await petalsWebsocket(ctx, arg, 'meta-llama/Llama-2-70b-chat-hf') # meta-llama/Llama-2-70b-hf
 
 async def GUANACO(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'timdettmers/guanaco-65b')
+    await petalsWebsocket(ctx, arg, 'timdettmers/guanaco-65b')
 
 async def LLAMA(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'huggyllama/llama-65b')
+    await petalsWebsocket(ctx, arg, 'huggyllama/llama-65b')
 
 async def BLOOMZ(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'bigscience/bloomz')
+    await petalsWebsocket(ctx, arg, 'bigscience/bloomz')
 
 async def CODELLAMA(ctx: commands.Context, arg: str):
-    await petal(ctx, arg, 'codellama/CodeLlama-34b-Instruct-hf')
+    await petalsWebsocket(ctx, arg, 'codellama/CodeLlama-34b-Instruct-hf')
 
 def PETALS() -> str:
     status = requests.get("https://health.petals.dev/api/v1/state").json()
