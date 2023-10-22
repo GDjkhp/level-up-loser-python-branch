fe9d0ec1c33cc4920a10fcefec14e08aa2d70e0c
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-10-22 18:45:06 +0800

    320kbps
    remove this later?

--- ytdlp_.py
+++ ytdlp_.py
@@ -42,6 +42,7 @@ def get_ydl_opts(arg):
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': arg,
+                'preferredquality': '320',
             }]
         }
     
