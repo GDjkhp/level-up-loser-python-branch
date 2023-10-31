e4cb261137f9e670a67d020c77f0eaeec24a177b
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-10-31 21:07:20 +0800

    removed if
    doesn't make any sense

--- weather.py
+++ weather.py
@@ -8,7 +8,6 @@ async def Weather(ctx: commands.Context, arg):
     except: return await message.edit(content="I tripped.")
     if "message" in results: return await message.edit(content=results["message"]) # rare
     c = f"{arg} ({results['temperature']}, {results['wind']})\n{results['description']}\n"
-    if results['forecast']:
-        for i in results['forecast']:
-            c += f"({i['temperature']}, {i['wind']}) "
+    for i in results['forecast']:
+        c += f"({i['temperature']}, {i['wind']}) "
     await message.edit(content=c)
